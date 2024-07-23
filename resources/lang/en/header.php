e63aa2d9a0df44395511d9cf986f83b8f190be4e
Fran Felicioni <ffelicioni@rica.design>
2024-07-23 22:59:28 +0200

    feat: add translations files

---
+++ resources/lang/en/header.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    "header_title" => "Laravel Weather App",
+    "header_description" => "Powered by ",
+];
---
+++ resources/lang/en/meta.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "home" => [
+        "title" => "Welcome to Weather App",
+        "description" => "Weather App is powered by Next Level Affiliate Marketing"
+    ],
+    "temperatures" => [
+        "title" => "Temperature Data for :city | Weather App",
+        "description" => "Displaying temperature data for your :city over time."
+    ]
+];
